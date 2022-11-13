5c5bc6fcbc6588ad44a0686bbf69c1b3c0b0f25f
Sidwebworks <xodedeveloper@gmail.com>
2022-11-14 00:52:25 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" width="200" class="logo" alt="Vite logo" />
+    </a>
+  </div>
+</template>
+
+
+
+
+<script setup lang="ts">
+import init, { prompt } from 'crate';
+
+init().then(() => {
+  console.log('init wasm-pack');
+  prompt("Hello Rust!")
+});
+
+</script>
+
